9d8585e7f5911f1a8c7e0d8ef5d9b652ef80f544
pedrobarbosa0 <47837747+pedrobarbosa0@users.noreply.github.com>
2020-06-10 06:11:57 -0300

    Changes in HTML, CSS and JS
    adding the modal of successfully registered, changing the name of the routes, and changing the forms

--- public/scripts/cadastro-concluido-modal.js
+++ public/scripts/cadastro-concluido-modal.js
@@ -1,5 +1,5 @@
 const buttonCreate = document.querySelector("form button")
-const sucessModal = document.querySelector("#deliveryman-modal")
+const sucessModal = document.querySelector("#cadastro-concluido")
 buttonCreate.addEventListener("click", () => {
     sucessModal.classList.remove("hide")
 })
